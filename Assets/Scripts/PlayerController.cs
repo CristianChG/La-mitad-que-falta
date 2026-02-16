049176d97db895311ef5c71afa2b1920bab44e11
CristianChG <chavezguiacristian@gmail.com>
2026-02-16 09:12:20 -0600

    feat: adding animations

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,18 +4,40 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 5f;
+
     private Vector2 input;
+    private Vector2 lastMoveDir;
+    private Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
 
     void Update()
     {
         input = GetMovementInput();
         Move();
+        UpdateAnimator();
     }
 
     void Move()
     {
         Vector3 movement = new Vector3(input.x, input.y, 0f);
         transform.position += movement * moveSpeed * Time.deltaTime;
+    }
+
+    void UpdateAnimator()
+    {
+        if (input != Vector2.zero)
+        {
+            lastMoveDir = input;
+
+            animator.SetFloat("moveX", input.x);
+            animator.SetFloat("moveY", input.y);
+        }
+
+        animator.SetBool("isMoving", input != Vector2.zero);
     }
 
     private Vector2 GetMovementInput()
@@ -25,12 +47,12 @@ public class PlayerController : MonoBehaviour
 
         if (keyboard != null)
         {
-            if (keyboard.aKey.isPressed || keyboard.leftArrowKey.isPressed) input.x -= 1;
-            if (keyboard.dKey.isPressed || keyboard.rightArrowKey.isPressed) input.x += 1;
-            if (keyboard.wKey.isPressed || keyboard.upArrowKey.isPressed) input.y += 1;
-            if (keyboard.sKey.isPressed || keyboard.downArrowKey.isPressed) input.y -= 1;
+            if (keyboard.aKey.isPressed || keyboard.leftArrowKey.isPressed) input.x = -1;
+            if (keyboard.dKey.isPressed || keyboard.rightArrowKey.isPressed) input.x = 1;
+            if (keyboard.wKey.isPressed || keyboard.upArrowKey.isPressed) input.y = 1;
+            if (keyboard.sKey.isPressed || keyboard.downArrowKey.isPressed) input.y = -1;
         }
 
-        return input.normalized;
+        return input; 
     }
 }
